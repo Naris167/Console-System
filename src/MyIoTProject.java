7e5aa4bfdfba39808e042fdaa027e0988890cdc7
Naris167 <narislyoko@gmail.com>
2022-05-02 00:26:01 +0700

    Week 1 - Printing LCD Screen with Basic Print Command (W1T3)

---
+++ src/MyIoTProject.java
@@ -0,0 +1,11 @@
+public class MyIoTProject {
+    public static void main(String[] args){
+        LCD.showMainMenu();
+        LCD.showLoginMenu();
+        LCD.showSystemMenu();
+        LCD.confirmRestart();
+        LCD.confirmShutdown();
+        LCD.showRestart();
+        LCD.showShutdown();
+    }
+}
